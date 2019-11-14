9094591949a06c9a8cc2c144fc7fa765b5b5dd77
Frido Koch <info@fridokoch.de>
2019-11-14 15:37:14 +0100

    Init

---
+++ model.go
@@ -0,0 +1,13 @@
+package gqlgen_bug
+
+type Todo struct {
+	ID   string `json:"id"`
+	Text string `json:"text"`
+	Done bool   `json:"done"`
+	User *User  `json:"user"`
+}
+
+type User struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
